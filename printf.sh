36f5810129b954b8a46758fc07388f5ab244c00e
a58982284 <a58982284@163.com>
2018-09-14 11:05:24 +0800

    Initial commit

---
+++ printf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+printf "%-5s %-10s %-4s\n" NO Name Mark
+printf "%-5s %-10s %-4.2f\n" 1 Sarath 80.3456
+printf "%-5s %-10s %-4.2f\n" 1 james 90.9989
+printf "%-5s %-10s %-4.2f\n" 1 jeff 7.564
